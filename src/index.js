1d35a972b0b2e55f66d6aca23fded27427ca10fd
paragrghoshal <ghoshsar000@students.centergrove.k12.in.us>
2026-05-20 23:41:36 -0400

    added securities

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { DiscordHono } from 'discord-hono'
 
-async function run(env, input) {
+async function run(env, user, input) {
   const response = await env.AI.run(
     "@cf/meta/llama-3.1-8b-instruct",
     {
@@ -10,8 +10,8 @@ async function run(env, input) {
           content: "You are a helpful Discord assistant named \"Da Boys AI\" in a discord server NAMED \"Da Boys\", you help users with their questions and provide information on a wide range of topics."
         },
         {
-          role: "user",
-          content: input
+          role: `user`,
+          content: `${user}: ${input}`
         }
       ]
     }
@@ -25,14 +25,18 @@ const app = new DiscordHono()
     c.resDefer(async (c) => {
       const msg = await c.followup("...");
 
-      try {
-        const result = await run(c.env, `${c.var.prompt}`);
-        await c.followup(`${result}`);
+      if (c.interaction.member.roles.includes("1506863348887847062") || c.interaction.member.roles.includes("1506863641545146368")) {
+        try {
+          const result = await run(c.env, c.interaction.member.user.username, `${c.var.prompt}`);
+          await c.followup(`${result}`);
 
-      } catch (err) {
-        console.error(err);
+        } catch (err) {
+          console.error(err);
 
-        await c.followup("Sorry, I didn't understand that. Try again please!");
+          await c.followup("Sorry, I didn't understand that. Try again please!");
+        }
+      } else {
+        await c.followup("Sorry, you don't have permission to use me!");
       }
     })
   );
